0b1fee4a3a452cd882ed180c9333b8c5f348c80b
Gabriel Alves <gabrieltots@gmail.com>
2018-11-03 13:46:30 -0300

    Decomposição LU pronta e código corrigido para comportar a numpy

--- t2_cn.py
+++ t2_cn.py
@@ -1,9 +1,11 @@
 import csv
 import numpy as np
 
+np.set_printoptions(precision=4)
+
 matrix_points = []
 matrix_polution = []
-points_solution = [23,7,30,-50,-12] #solução do sistema de pontos amostrais 23,7,30,-50,-12
+global points_solution #solução do sistema de pontos amostrais 23,7,30,-50,-12
 polution_solution = [0,0,0,0,0] #solução do sistema poluição 22,17,11,72,4
 
 try_pts = [] #chute inicial para os pontos amostrais
@@ -37,10 +39,13 @@ def first_values():
             try_plt.append(matrix_polution[i][n]/matrix_polution[i][i])
 
 def sort_lists():
+    global points_solution
+    points_solution = decLU(matrix_points)[3]
+    #print(points_solution)
     for i in range(n):
         for j in range(0, n-i-1):
-           if points_solution[j] > points_solution[j+1]:
-               points_solution[j], points_solution[j+1] = points_solution[j+1], points_solution[j]
+           if points_solution[j,0] > points_solution[j+1,0]:
+               points_solution[j,0], points_solution[j+1,0] = points_solution[j+1,0], points_solution[j,0]
                polution_solution[j], polution_solution[j+1] = polution_solution[j+1], polution_solution[j]
 
 ### Função para dados de saída ### 
@@ -50,7 +55,7 @@ def saida():
     print ('|\tPontos  \t|\tPoluição\t|')
     print('-------------------------------------------------')
     for i in range(0,n):
-        print('|\t{:.4f}  \t|\t{:.4f}\t\t|'.format(points_solution[i], polution_solution[i]))
+        print('|\t{:.4f}  \t|\t{:.4f}\t\t|'.format(points_solution[i,0], polution_solution[i]))
 
     print('-------------------------------------------------')
     
@@ -67,28 +72,15 @@ def decLU(A):
                 U[i,k] = U[i,k] - (L[i,j] * U[j,k])
             U[i,j] = 0
 
-    print('U >>\n{}'.format(U))
-    print()
-    print('L >>\n{}'.format(L))
-    print()
-
     b = np.delete(U, np.s_[0:n], 1)
-    print('b >>\n{}'.format(b))
-    print()
     
     newU = np.delete(U, np.s_[n], 1)
-    print('newU >>\n{}'.format(newU))
-    print()
 
     y = np.linalg.solve(L,b)
-    print('y >>\n{}'.format(y))
-    print()
 
     x = np.linalg.solve(newU,y)
-    print('x >>\n{}'.format(x))
-    print()
 
-    #return L, U
+    return L, newU, y, x
 
 
 ###    Lagrange    ###
@@ -98,7 +90,7 @@ def calculates_Lk(x, k):
     for i in range(0,n):
         if i == k:
             continue
-        lk = lk * (x - points_solution[i])/(points_solution[k]-points_solution[i])
+        lk = lk * (x - points_solution[i,0])/(points_solution[k,0]-points_solution[i,0])
 
     return lk
 
@@ -115,6 +107,7 @@ def main():
     op = 0
     first_values()
     sort_lists()
+    #print(points_solution)
 
     while True:
         print('------------------------------------------')
@@ -128,8 +121,11 @@ def main():
         if op == 1:
             saida()
         elif op == 2:
-            #decLU(matrix_points)
-            decLU(matrix_points)
+            ans = decLU(matrix_points)
+            print('###    L >> \n{}\n'.format(ans[0]))
+            print('###    U >> \n{}\n'.format(ans[1]))
+            #print('###    y >> \n{}\n'.format(ans[2]))
+            #print('###    x >> \n{}\n'.format(ans[3]))
         elif op == 3:
             print('nothing to do yet')
         elif op == 4:
