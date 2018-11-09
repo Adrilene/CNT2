a5b3c5499a648b8602052883a14c0b69947b794f
Gabriel Alves <sousa.alves@aluno.uece.br>
2018-11-09 15:01:07 -0300

    Adicionado Gauss Seidel

--- t2_cn.py
+++ t2_cn.py
@@ -5,21 +5,15 @@ np.set_printoptions(precision=4)
 
 n = 5 
 
-matrix_points = []
-matrix_polution = []
+matrix_points = [] #matriz A
+matrix_polution = [] #matriz B 
 
 
-global points_solution #solução do sistema de pontos amostrais 23,7,30,-50,-12
+global points_solution  # Armazena solução do S1
 
-polution_solution = [0,0,0,0,0] #solução do sistema poluição 22,17,11,72,4
-
-try_pts = [] #chute inicial para os pontos amostrais
-try_plt = [] #chute inicial para a poluição
-
+global polution_solution # Armazena a solução do S2
+polution_solution = [0 for i in range(n)]
 n = 5 #tamanho da matriz
-
-e = 10e-4
-
 
 ### ler matrizes ###
 with open('matrix.csv', newline='') as csvfile: 
@@ -39,7 +33,7 @@ def sort_lists():
 
     global points_solution
     points_solution = decLU(matrix_points)[3]
-    #print(points_solution)
+    #print(points_solution) 
     for i in range(n):
         for j in range(0, n-i-1):
            if points_solution[j,0] > points_solution[j+1,0]:
@@ -50,9 +44,8 @@ def sort_lists():
 ### Função para dados de saída ### 
 def saida(): 
 
-
     sort_lists()
-
+    
 
     print('-------------------------------------------------')
     print ('|\tPontos  \t|\tPoluição\t|')
@@ -66,7 +59,7 @@ def saida():
     
 
 ###    Decomposição LU    ###
-def decLU(A):
+def decLU():
     U = np.array(matrix_points)
     L = np.eye(n)
     
@@ -87,6 +80,85 @@ def decLU(A):
 
     return L, newU, y, x
 
+
+### Método Gauss-Seidel ###
+
+def sassenfeld():
+    b = [1 for i in range(n)]
+    b[0] = 0
+
+    for i in range(1,n): 
+        b[0] = b[0] + matrix_polution[0][i]
+
+    b[0] = b[0]/matrix_polution[0][0]
+
+    for i in range(1,n):
+        for j in range(n):
+            if j == i:
+                continue
+            for k in range(j-1,-1,-1):
+                b[i] = matrix_polution[i][j] * b[k]
+            b[i] = b[i]/matrix_polution[j][j]
+    
+    if max(b) >= 1:
+        return False
+    return True
+
+### Calcula Chute inicial para Matriz de Poluição (B) ###
+
+def chuteInicial():
+    x = []
+    for i in range(n): 
+        if matrix_polution[i][0] != 0:
+            x.append(matrix_polution[i][n]/matrix_polution[i][0])
+        else: 
+            x.append(0)
+
+    return x
+
+def calculaErro(previous):
+    
+    maxDiff = 0 #Armazenará o máximo da diferença entre os resultados
+    diff = 0 #Auxiliar
+    maxSol = max(polution_solution) #Armazena o máximo da solução atual
+
+    if maxSol == 0: #Se o máximo da solução for zero, retorna 1, pois não pode dividir por zero.
+        return 1
+    
+    for i in range(n):
+        diff = abs(polution_solution[i] - previous[i])
+        
+        if diff > maxDiff:
+            maxDiff = diff
+    
+    return maxDiff/maxSol
+
+
+#Recebe a matriz B, o vetor resposta b, o "chute" inicial, elementos na resposta, e o erro.
+def gaussSeidel(erro, maxInt): 
+    
+    previousX = chuteInicial() #Recebe o chute inicial
+
+    k = 0 
+    
+    while(k<maxInt and calculaErro(previousX)>erro):
+        for i in range(n):
+            previousX[i] = polution_solution[i]
+            soma = 0 
+
+            for j in range(n):
+                if j == i: 
+                    continue
+                soma = soma + matrix_polution[i][j] * polution_solution[j]
+
+            polution_solution[i] = (matrix_polution[i][n]-soma)/matrix_polution[i][i]
+
+        print('Solução na iteração {}: {}'.format(k,polution_solution))
+        print('Erro: {}'.format(calculaErro(previousX)))
+        print('\n')
+
+        k = k+1
+
 ###    Lagrange    ###
 
 def calculates_Lk(x, k):
@@ -98,7 +170,6 @@ def calculates_Lk(x, k):
 
         lk = lk * (x - points_solution[i,0])/(points_solution[k,0]-points_solution[i,0])
 
-
     return lk
 
 def result_Polinomio(x): 
@@ -108,8 +179,6 @@ def result_Polinomio(x):
         result += polution_solution[i]*calculates_Lk(x,i)
 
     return result
-
-### Fim Lagrange ### 
 
 def main(): 
     
@@ -125,17 +194,28 @@ def main():
         print('4 - Calcula poluição (Interpolação Lagrange)')
         print('5 - Sair')
         op = int(input('Digite a opção: '))
+        print('\n')
 
         if op == 1:
-            saida()
+            if max(points_solution) == 0: 
+                print('Favor, resolver o S1 primeiro. ')
+            elif max(polution_solution) == 0:
+                print('Favor, resolver o S2 primeiro.')
+            else:
+                saida()
         elif op == 2:
             ans = decLU(matrix_points)
             print('###    L >> \n{}\n'.format(ans[0]))
             print('###    U >> \n{}\n'.format(ans[1]))
-            #print('###    y >> \n{}\n'.format(ans[2]))
-            #print('###    x >> \n{}\n'.format(ans[3]))
+            print('###    y >> \n{}\n'.format(ans[2]))
+            print('###    x >> \n{}\n'.format(ans[3]))
         elif op == 3:
-            print('nothing to do yet')
+            if sassenfeld():
+                e = float(input('Erro: '))
+                maxInt = int(input('Número máximo de Interações: '))
+                gaussSeidel(e,maxInt)
+            else:
+                print('A matriz não irá convergir.\n')
         elif op == 4:
             x = int(input('Valor do ponto: '))
             print('O grau de poluição é de {:.4f}.\n'.format(polinomio(x)))
